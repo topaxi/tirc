ad269894506d70782915c07f5660e9e353384c7f
Damian Senn <damian.senn@topaxi.codes>
2024-03-05 21:29:03 +0100

    fix: better tirc.on types

--- lua/tirc/init.lua
+++ lua/tirc/init.lua
@@ -1,9 +1,12 @@
 ---@alias EventName 'message' | 'format-time' | 'format-message' | 'format-user'
 
+---@class TircSender
+---@field send_privmsg fun(target: string, message: string)
+---@field send_notice fun(target: string, message: string)
+
 ---@class TircModule
 ---@field version string
----@field create_config fun(): TircConfig
----@field on fun(event_name: EventName, callback: function)
+---@field on fun(event_name: EventName, callback: fun(msg: table, irc: TircSender))
 local M = require('_tirc')
 
 function M.create_config()
